f2ea6a7038fef32abe0d8703ac4eb86370b328ae
rida <elbahtouri0123@gmail.com>
2021-02-26 10:51:15 +0100

    style movie detail page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -17,27 +17,26 @@ const  Movie=(props)=> {
     
     const renderMovie=(m)=> {
         if(m.title){
-            console.log(m)
             return (
-                <div>
-                <h1 className="text-white text-lg capitalize font-bold"> {m.title} </h1>
-                <div className="relative">
-                <img className="rounded-t-lg" src={`https://image.tmdb.org/t/p/w500${m.poster_path}`} alt={m.title} />
+                <div className="text-center mx-auto p-5 max-w-6xl">
+                <h1 className="text-white text-2xl capitalize font-bold"> {m.title} </h1>
+                <div className="relative max-w-lg mx-auto">
+                <img className="rounded-t-lg w-full " src={`https://image.tmdb.org/t/p/w500${m.poster_path}`} alt={m.title} />
                 <span className="absolute bg-gray-600 bg-opacity-50 px-2 py-1 top-4 left-0 rounded-r-lg text-gray-50"><FontAwesomeIcon icon={faStar} /> 
                 {m.vote_average}({m.vote_count}) </span>
                 </div>
                 
-                <p> Release Date :
+                <p className="text-gray-100 text-md capitalize"> Release Date :
                 <span>
                 {m.release_date.slice(8,10)}
-</span> <span>
+                </span> <span>
                 {months[parseInt(m.release_date.slice(5,7),10)-1]}
                </span> <span>
                 {m.release_date.slice(0,4)}</span>  </p>
-
-                <ul>
+                <p className="text-gray-100 text-md capitalize">Genres</p>
+                <ul className="flex justify-center">
                     {m.genres.map(g=>{
-                        return <li key={g.id}>{g.name}</li>
+                        return <li className="bg-blue-400 text-gray-50 rounded-md p-1 mr-2" key={g.id}>{g.name}</li>
                     })}
                 </ul>
                 <p className="text-gray-100">{m.overview}</p>
@@ -48,8 +47,7 @@ const  Movie=(props)=> {
     }
 
     return (
-        <div>
-    <h1>Movie Detail</h1>   
+        <div> 
     {renderMovie(props.Movie)}         
         </div>
     )
